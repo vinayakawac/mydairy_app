b836e573c6c3df49303958f61f7505eabce76bbf
Vinayaka H Shankara <115161801+vinayakawac@users.noreply.github.com>
2026-03-17 21:01:04 +0530

    .xb2update

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_mydairy_app_MyDiaryApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.mydairy_app.MyDiaryApp"
-)
-public final class _com_example_mydairy_app_MyDiaryApp {
-}
